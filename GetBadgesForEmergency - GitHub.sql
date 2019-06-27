8d9b129e8ff673616e256c8e4cd229370e583ebe
Ashley Stanbridge <36490302+AshleyStanbridge96@users.noreply.github.com>
2019-06-27 10:05:14 -0500

    SQL code from SQL Server

---
+++ GetBadgesForEmergency - GitHub.sql
@@ -0,0 +1,21 @@
+  WITH BI AS
+(
+  SELECT BADGE_C.CARDNO, BADGE_V.BADGE_EMPLOYEE_NO
+  FROM BADGE_V 
+  INNER join BADGE_C ON BADGE_C.ID = BADGE_V.ID
+),
+empScans as 
+(select evnt_dat, location, CARDNO, fname, lname, PANEL_DESCRP
+from EV_LOG
+where(EV_LOG.[EVNT_DAT] > GETDATE() - 1)
+and (EVNT_DAT is not null)
+and (LNAME is not null)
+and (upper (PANEL_DESCRP) like 'Pel%')
+group by CARDNO, LOCATION, fname, lname, EVNT_DAT, PANEL_DESCRP
+)
+select * from empScans
+inner join BI on BI.cardno = empScans.CARDNO
+where (evnt_dat = (select max(evnt_dat) from empScans where empScans.CARDNO = BI.CARDNO
+and evnt_dat > '20190625 06:00:00'	--Start Date
+and evnt_dat < '20190625 17:00:00')) --End Date
+order by lname asc
