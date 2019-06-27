10ed702b413635648c18fac47d8d2045b7fb5960
Ashley Stanbridge <36490302+AshleyStanbridge96@users.noreply.github.com>
2019-06-27 10:12:57 -0500

    Comments explaining process of CTE's and main query

--- GetBadgesForEmergency - GitHub.sql
+++ GetBadgesForEmergency - GitHub.sql
@@ -1,11 +1,14 @@
   WITH BI AS
 (
+--This CTE is taking an employee's card number and their employee number then joining the two
   SELECT BADGE_C.CARDNO, BADGE_V.BADGE_EMPLOYEE_NO
   FROM BADGE_V 
   INNER join BADGE_C ON BADGE_C.ID = BADGE_V.ID
 ),
 empScans as 
-(select evnt_dat, location, CARDNO, fname, lname, PANEL_DESCRP
+(
+--This CTE is primarily selecting the information needed for the report
+select evnt_dat, location, CARDNO, fname, lname, PANEL_DESCRP
 from EV_LOG
 where(EV_LOG.[EVNT_DAT] > GETDATE() - 1)
 and (EVNT_DAT is not null)
@@ -13,6 +16,8 @@ and (LNAME is not null)
 and (upper (PANEL_DESCRP) like 'Pel%')
 group by CARDNO, LOCATION, fname, lname, EVNT_DAT, PANEL_DESCRP
 )
+--This query is tying the CTE's together. There is a subquery for evnt_dat to select the most recent employee scan on the selected
+--date range
 select * from empScans
 inner join BI on BI.cardno = empScans.CARDNO
 where (evnt_dat = (select max(evnt_dat) from empScans where empScans.CARDNO = BI.CARDNO
